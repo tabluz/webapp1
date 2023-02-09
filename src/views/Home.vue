2a18583d0ff7778dbd909664632987805e11d93a
Daniel Moran <skaan.dmv@gmail.com>
2023-02-08 20:19:16 -0500

    Diseño del dashboard del sistema

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,7 +28,7 @@ const { getEsential } = useAuthStore();
           <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
             <dl class="mb-0">
               <dt class="fs-3 fw-bold">32</dt>
-              <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Pending Orders</dd>
+              <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Alumnos Registrados</dd>
             </dl>
             <div class="item item-rounded-lg bg-body-light">
               <i class="far fa-gem fs-3 text-primary"></i>
@@ -36,7 +36,7 @@ const { getEsential } = useAuthStore();
           </div>
           <div class="bg-body-light rounded-bottom">
             <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-              <span>View all orders</span>
+              <span>Lista de Alumnos</span>
               <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
             </a>
           </div>
@@ -49,7 +49,7 @@ const { getEsential } = useAuthStore();
           <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
             <dl class="mb-0">
               <dt class="fs-3 fw-bold">124</dt>
-              <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">New Customers</dd>
+              <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Profesores Registrados</dd>
             </dl>
             <div class="item item-rounded-lg bg-body-light">
               <i class="far fa-user-circle fs-3 text-primary"></i>
@@ -57,7 +57,7 @@ const { getEsential } = useAuthStore();
           </div>
           <div class="bg-body-light rounded-bottom">
             <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-              <span>View all customers</span>
+              <span>Lista de Profesores</span>
               <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
             </a>
           </div>
@@ -70,7 +70,7 @@ const { getEsential } = useAuthStore();
           <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
             <dl class="mb-0">
               <dt class="fs-3 fw-bold">45</dt>
-              <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Messages</dd>
+              <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Administradores</dd>
             </dl>
             <div class="item item-rounded-lg bg-body-light">
               <i class="far fa-paper-plane fs-3 text-primary"></i>
@@ -78,7 +78,7 @@ const { getEsential } = useAuthStore();
           </div>
           <div class="bg-body-light rounded-bottom">
             <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-              <span>View all messages</span>
+              <span>Lista de Administradores</span>
               <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
             </a>
           </div>
